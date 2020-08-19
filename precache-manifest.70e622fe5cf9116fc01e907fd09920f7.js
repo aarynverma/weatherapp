1f27ab93d40caabe6609d674b645d8650b013bbf
aarynverma <18100BTCSE02667@svvvindore.onmicrosoft.com>
2020-08-19 22:51:08 +0530

    Updates

---
+++ precache-manifest.70e622fe5cf9116fc01e907fd09920f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ab81561fe9be1e6fbcdda0d6398bb95",
+    "url": "/weatherapp/index.html"
+  },
+  {
+    "revision": "e7ee03c3b066a5c04c2e",
+    "url": "/weatherapp/static/css/main.950cb3d6.chunk.css"
+  },
+  {
+    "revision": "1c6f6ed65d6c23fc0ed9",
+    "url": "/weatherapp/static/js/2.9a4b92c4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weatherapp/static/js/2.9a4b92c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7ee03c3b066a5c04c2e",
+    "url": "/weatherapp/static/js/main.94cec6b7.chunk.js"
+  },
+  {
+    "revision": "8862584b26a768bb4b89",
+    "url": "/weatherapp/static/js/runtime-main.cb4f0870.js"
+  }
+]);
